a8c2a63c418280166ecf37b6255f51d0fdb71345
Mohammed Arsh Khan <20bcs138@ietdavv.edu.in>
2023-10-24 20:31:44 +0530

    7th commit

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -20,6 +20,7 @@ const Product = (props) => {
 			setCount("productCount", Number(count.productCount) + 1, { expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000) });
 			if (auth) {
 				const updatedCart = await axios.post("http://localhost:4000/api/cart", { _id: user._id, product: count.productCount + 1 }, { withCredentials: true });
+				setCount("productCount", Number(count.productCount) + 1, { maxAge: 30 * 24 * 60 * 60 });
 				const { password, ...details } = updatedCart.data;
 				dispatch(setUser(details));
 			}
